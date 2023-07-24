e0fb718a6cd5f8a462189bfba2a89580861229ba
Jiwon Kim <74237427+jiwon-km@users.noreply.github.com>
2023-07-24 21:09:11 +0900

    Create 정렬-최솟값만들기.py

---
+++ 프로그래머스/정렬-최솟값만들기.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12941
+
+def solution(A,B):
+    answer = 0
+    A.sort()
+    B.sort(reverse=True)
+    
+    for i in range(len(A)):
+        answer += A[i] * B[i]
+    
+    return answer
